124f20c2fac0a05b2976f74fe46a1863f3b6a39a
Fernando Reyes <fernando.reyes@alumnos.uach.cl>
2019-01-23 16:19:02 -0300

    arreglos menores

--- src/app/components/profesor/p-clases-agregaralumno/p-clases-agregaralumno.component.ts
+++ src/app/components/profesor/p-clases-agregaralumno/p-clases-agregaralumno.component.ts
@@ -9,29 +9,27 @@ import { LoginService } from 'src/app/services/login.service';
   styleUrls: ['./p-clases-agregaralumno.component.css']
 })
 export class PClasesAgregaralumnoComponent implements OnInit {
-  idclase: any;
   constructor(public http: HttpClient,
-    public _LoginService: LoginService) { 
-    this.idclase = localStorage.getItem('id_desafio');
-    
-  }
+    public _LoginService: LoginService) { }
   agregarAlumno(idclase, alumno){
+    console.log(idclase, alumno);
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json',
         'Authorization': 'JWT ' + localStorage.getItem('token')
       })
     };
-    let data = {class: Number(idclase), student: alumno};
+    let data = {class: idclase, student: alumno};
     const req = this.http.post(this._LoginService.getUrlApi()+ 'class/add_student', data, httpOptions)
     .subscribe(
       res => {
         console.log(res)
-        if (res['codigo'] === 200) {
+        if (res['code'] === 200) {
           console.log('Estudiante agregado exitosamente');
-        } else { console.log('Algo salio mal');}
-      },
-      err => {
+        } else {
+          console.log(res);
+          console.log('Algo salio mal');
+        }
       }
     );
   }
