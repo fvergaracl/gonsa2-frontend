7b51cc42064801e69cc54258ca2ea975eacb644b
Jorge Obando <sebita_118@hotmail.com>
2019-01-31 15:55:29 -0300

    carousel y configuracion  para estudiante

--- src/app/components/estudiante/e-tareas/e-tareas.component.ts
+++ src/app/components/estudiante/e-tareas/e-tareas.component.ts
@@ -13,47 +13,7 @@ import { Observable, interval } from 'rxjs';
   styleUrls: ['./e-tareas.component.css']
 })
 export class ETareasComponent implements OnInit {
-//   tareascate:any[]=[];
-//   constructor(public http: HttpClient, public router: Router, public _LoginService: LoginService ) { }
-//   getTareas() {
-//     let tareas = [];
-//     console.log(localStorage.getItem('token'));
-//     const httpOptions = {
-//       headers: new HttpHeaders({
-//         'Content-Type': 'application/json',
-//         'Authorization': 'JWT ' + localStorage.getItem('token')
-//       })
-//     };
-//     this.http.get( this._LoginService.getUrlApi() + '/getallchallenges/matematicas', httpOptions).subscribe(data => {
-//       if (data['code'] === 200) {
-//         for (let i = 0; i < data['challenges'].length; i++) {
-//           let id = data['challenges'][i][0];
-//           let idto = data['challenges'][i][1];
-//           let ti = data['challenges'][i][2];
-//           let ur = data['challenges'][i][3];
-//           let re = data['challenges'][i][4];
-//           let ob = data['challenges'][i][5];
-//           let fe = data['challenges'][i][6];
-//           let mo = data['challenges'][i][7];
-//           let ca = data['challenges'][i][8];
-//           let item ={id:id, idtoken: idto, titulo:ti, url:ur, resumen:re, objetivo:ob, fecha:fe, modificacion: mo, categoria: ca};
-//           tareas.push(item);
-//         }
-//         console.log(tareas);
-//         localStorage.setItem('tareas', JSON.stringify(tareas));
-//      } else {
-//       console.log(data['code']);
-//      }
-//     });
-
-//   return tareas;
-//   }
-//   ngOnInit() {
-//     this.tareascate = this.getTareas();
-//   }
-
-// }
-
+  tareascate:any[]=[];
 @Input() name: string;
 imgags = [
   'assets/bg.jpg',
@@ -62,8 +22,9 @@ imgags = [
   'assets/holi.jpg'
 ];
 public carouselTileItems$: Observable<number[]>;
+tareas: any;
 public carouselTileConfig: NguCarouselConfig = {
-  grid: { xs: 1, sm: 1, md: 1, lg: 5, all: 0 },
+  grid: { xs: 3, sm: 3, md: 3, lg: 3, all: 0 },
   speed: 250,
   point: {
     visible: true
@@ -75,13 +36,59 @@ public carouselTileConfig: NguCarouselConfig = {
 };
 tempData: any[];
 
-constructor(private cdr: ChangeDetectorRef) {}
+constructor(public http: HttpClient, public router: Router, public _LoginService: LoginService, private cdr: ChangeDetectorRef ) { }
+
+getTareas() {
+    this.tareas = [];
+    console.log(localStorage.getItem('token'));
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'JWT ' + localStorage.getItem('token')
+      })
+    };
+    this.http.get( this._LoginService.getUrlApi() + '/getallchallenges/matematicas', httpOptions).subscribe(data => {
+      if (data['code'] === 200) {
+        for (let i = 0; i < data['challenges'].length; i++) {
+          let id = data['challenges'][i][0];
+          let idto = data['challenges'][i][1];
+          let ti = data['challenges'][i][2];
+          let ur = data['challenges'][i][3];
+          let re = data['challenges'][i][4];
+          let ob = data['challenges'][i][5];
+          let fe = data['challenges'][i][6];
+          let mo = data['challenges'][i][7];
+          let ca = data['challenges'][i][8];
+          let item ={id:id, idtoken: idto, titulo:ti, url:ur, resumen:re, objetivo:ob, fecha:fe, modificacion: mo, categoria: ca};
+          this.tareas.push(item);
+        }
+        console.log(this.tareas);
+        localStorage.setItem('tareas', JSON.stringify(this.tareas));
+     } else {
+      console.log(data['code']);
+     }
+    });
+    let tareas2 = this.tareas;
+
+  return tareas2;
+  }
+//   ngOnInit() {
+//     this.tareascate = this.getTareas();
+//   }
+
+// }
+
+
+
+// constructor(private cdr: ChangeDetectorRef) {}
 
 ngOnInit() {
+  let asd=  JSON.parse(localStorage.getItem('tareas'));
+  let asd2= Object.keys(asd).length;
   this.tempData = [];
   this.carouselTileItems$ = interval(500).pipe(
     startWith(-1),
-    take(6),
+    take(asd2),
     map(val => {
       const data = (this.tempData = [
         ...this.tempData,
@@ -90,6 +97,7 @@ ngOnInit() {
       return data;
     })
   );
+  this.tareascate = this.getTareas();
 }
 
 }
