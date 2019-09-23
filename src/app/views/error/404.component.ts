c4ff6a8e8ad50116b08f072ebadd00e0dbfac839
Fabián Catalán <f.catalan.v@gmail.com>
2019-09-23 00:38:41 -0300

    404 Style changes

--- src/app/views/error/404.component.ts
+++ src/app/views/error/404.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: '404.component.html'
+  templateUrl: '404.component.html',
+  styleUrls: ['404.scss']
 })
-export class P404Component {
-
-  constructor() { }
-
+export class P404Component implements OnInit{
+  time: any =5;
+  constructor(private router: Router) { }
+  
+  ngOnInit(){
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 5000);   
+  }
 }
